584923f5daf840ae05a6a76bcfacb8cde0778c7f
KYS <97yeonsoo@gmail.com>
2024-01-04 15:52:42 +0900

    admin

--- src/main/java/com/project/naebang/config/SecurityConfig.java
+++ src/main/java/com/project/naebang/config/SecurityConfig.java
@@ -1,4 +1,12 @@
 package com.project.naebang.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+
 }
